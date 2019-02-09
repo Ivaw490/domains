e8f1e5b91ca4b82ffd67381c653ab9b5c65e1945
Ivan dyachenko <43813204+Ivaw490@users.noreply.github.com>
2019-02-09 20:14:32 +0700

    rename menu - task3 link

--- PhpLes6/templates/menu.php
+++ PhpLes6/templates/menu.php
@@ -13,7 +13,7 @@
     <ul class="menu">
         <li><a href="/task.php?task=1">Task 1</a></li>
         <li><a href="/task.php?task=2">Task 2</a></li>
-        <li><a href="/task.php?task=3">Task 3</a></li>
+        <li><a href="/task3.php">Task 3</a></li>
         <li><a href="/task4.php">Task 4</a></li>
     </ul>
 </div>
