542b77be4d5b8db7dce636e5142b8def0d1dd86e
Ivan dyachenko <43813204+Ivaw490@users.noreply.github.com>
2019-02-09 20:10:03 +0700

    rename css links

--- PhpLes6/templates/task1.php
+++ PhpLes6/templates/task1.php
@@ -5,11 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/styles/task1-3.css">
+    <link rel="stylesheet" href="/styles/task.css">
     <title>Task 1</title>
 </head>
 <body>
-<form action="">
+<form action="/task.php?$task=1">
     <input type="text" name="first-num">
     <select name="operation" id="select-operation">
         <option value="+">+</option>
@@ -19,7 +19,7 @@
     </select>
     <input type="text" name="second-num">
     <input type="submit" value="=">
-    <label id="result" value=<?=$result?>><?=" = " . $result?></label>
+    <label id="result" value=<?=$result?>><?=$result?></label>
     <a href="/index.php" class="menu-btn">Назад в меню</a>
 </form>
 </body>
