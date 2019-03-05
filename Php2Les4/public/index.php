0964a94a5ac4b6fc184f50332dc163c3f8a0d6c0
Ivan dyachenko <43813204+Ivaw490@users.noreply.github.com>
2019-03-05 20:43:35 +0700

    inserting new added img to bd

--- Php2Les4/public/index.php
+++ Php2Les4/public/index.php
@@ -6,10 +6,20 @@ include EXTERNALS_DIR . "/resize.php";
 include ENGINE_DIR . "/class/bd.class.php";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $filename = PUB_DIR . "img/" . $_FILES["img"]["name"];
-    uploadFile($filename, 'img');
-    resize($filename, PUB_DIR . "img/mini/" . $_FILES["img"]["name"], 200, 125);
-    header("Location: /index.php");
+    $conn = bd::getConnection();
+    $filename = $_FILES["img"]["name"]; //оригинальное имя изображения
+    $path = PUB_DIR ."img/" . $_FILES["img"]["name"];//абсолютный путь до папки хранения полноразмерного изображения
+    $path_mini = PUB_DIR ."img/mini/" . $_FILES["img"]["name"];//абсолютный путь до папки хранения уменьшенного изображения
+    $size = $_FILES["img"]["size"];//размер загружаемого изображения
+    uploadFile($path, "img");//помещаем изображение в $path
+    resize($path, $path_mini, 200, 131);//создаем уменьшеную версию изображения и кладем ее в $path_mini
+    $sql = "INSERT INTO gallery.images_mini (name, path) VALUES ('$filename', '/img/mini/')";
+    $conn->exec($sql);//загружаем уменьшенное изображение в БД
+    $last_mini_id = $conn->lastInsertId();//id последнего добавленного уменьшенной изображения в БД
+    $sql = "INSERT INTO gallery.images (name, path, size, mini_id) 
+            VALUES ('$filename', '/img/', '$size', '$last_mini_id')";
+    $conn->exec($sql);//загружаем полноразмерное изображение в БД
+    header("Location: /index.php");//заголовок с перенаправлением, для нормальной работы обновления страницы
 }
 
 $img_gallery = getGallery();
